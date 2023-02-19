c0acd78a8d77e09d7f34d33fc316594a68c2137d
Seungho <poream3387@gmail.com>
2023-02-19 14:38:11 +0900

    Add Working Directory to CI/CD

--- backend/hello_efs/app.py
+++ backend/hello_efs/app.py
@@ -4,6 +4,6 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "Message": "Hello Capstone!"
+            "Message": "Hello Seungho!!"
         }),
     }
