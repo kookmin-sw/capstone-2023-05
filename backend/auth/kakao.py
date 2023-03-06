1e871d135d35efae79151c9816d4f6cbf2d1be28
ChangHoon Sung <changhoon.dev@gmail.com>
2023-03-06 10:55:36 +0900

    Add example of retrieving email from kakao

---
+++ backend/auth/kakao.py
@@ -0,0 +1,34 @@
+import requests
+import os
+
+def get_email(code):
+    """
+    Trying to login through Kakao account\n
+    You should replace those things below\n
+    - Kakao application: client_id(REST API Key), client_secret, redirect_uri\n
+    - AWS Cognito: ClientId(User Pool application client ID)\n
+    :return:
+    """
+
+    client_id = os.getenv("KAKAO_REST_API_KEY")
+    client_secret = os.getenv("KAKAO_CLIENT_SECRET")
+    #TODO: Change redirect_uri to env variable
+    redirect_uri = f'http://localhost:3000/dev/login/kakao'
+
+    parameters = {
+        'grant_type': 'authorization_code',
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'redirect_uri': redirect_uri,
+        'code': code
+    }
+    response = requests.post("https://kauth.kakao.com/oauth/token", params=parameters)
+    print(response.json())
+    access_token = response.json()['access_token']
+
+    headers = {'Authorization': f'Bearer {access_token}'}
+    user_information = requests.get("https://kapi.kakao.com/v2/user/me", headers=headers).json()
+    print("User information: ", user_information)
+    user_email = user_information['kakao_account']['email']
+    
+    return user_email
--- backend/functions/app.py
+++ backend/functions/app.py
@@ -4,6 +4,7 @@ import requests
 import json
 import boto3
 import botocore.errorfactory
+import auth.kakao as kakao 
 
 
 def kakao_login(event, context):
@@ -14,56 +15,43 @@ def kakao_login(event, context):
     - AWS Cognito: ClientId(User Pool application client ID)\n
     :return:
     """
-    # Phase 1: Get authorized code from Kakao
-    # parsed = urlparse.urlparse(request.full_path)
-    # temp_code = parse_qs(parsed.query)['code']
 
-    # From chatGPT:
-    temp_code = event['code']
-    print("Access code: ", temp_code)
+    #Get authorized code from Kakao
+    access_code = event['queryStringParameters']['code']
+    email = kakao.get_email(access_code)
 
-    # Phase 2: Get access token from Kakao
-    parameters = {
-        'grant_type': 'authorization_code',
-        'client_id': '${KAKAO_REST_API_KEY}',
-        'client_secret': '${KAKAO_CLIENT_SECRET}',
-        'redirect_uri': 'https://${CUSTOM_DOMAIN}/login/kakao',
-        'code': temp_code
-    }
-    response = requests.post("https://kauth.kakao.com/oauth/token", params=parameters, verify=False)
-    kakao_access_token = response.json()['access_token']
-    # print("Access token: ", kakao_access_token)
-
-    # Phase 3: Get user's email
-    header = {'Authorization': f'Bearer {kakao_access_token}'}
-    user_information = requests.get("https://kapi.kakao.com/v2/user/me", headers=header, verify=False).json()
-    user_email = user_information['kakao_account']['email']
-    # print(user_email)
-
-    # Phase 4: Add user to Cognito User Pool
-    client = boto3.client('cognito-idp', region_name='ap-northeast-2',
-                          aws_access_key_id='${AWS_ACCESS_KEY}',
-                          aws_secret_access_key='${AWS_ACCESS_SECRET}')
-    try:
-        client.admin_create_user(
-            UserPoolId='ap-northeast-2_Gx5e0MS4K',
-            Username=user_email,
-            UserAttributes=[
-                {
-                    'Name': 'email',
-                    'Value': user_email
-                }
-            ]
-        )
-    except botocore.errorfactory.ClientError as client_error:
-        if client_error.response['Error']['Code'] == "UsernameExistsException":
-            print("Already signed with Kakao!")
-        else:
-            raise client_error
-
-    body = {
-        "message": "Hello! Your email is %s" % user_email,
-        "input": event
+    # return email for test
+    return {
+       "statusCode": 200,
+        "body": json.dumps(email)
     }
 
-    return {"statusCode": 200, "body": json.dumps(body)}
+    # return {"statusCode": 200, "body": json.dumps(email)}
+
+    # # Phase 4: Add user to Cognito User Pool
+    # client = boto3.client('cognito-idp', region_name='ap-northeast-2',
+    #                       aws_access_key_id='${AWS_ACCESS_KEY}',
+    #                       aws_secret_access_key='${AWS_ACCESS_SECRET}')
+    # try:
+    #     client.admin_create_user(
+    #         UserPoolId='ap-northeast-2_Gx5e0MS4K',
+    #         Username=user_email,
+    #         UserAttributes=[
+    #             {
+    #                 'Name': 'email',
+    #                 'Value': user_email
+    #             }
+    #         ]
+    #     )
+    # except botocore.errorfactory.ClientError as client_error:
+    #     if client_error.response['Error']['Code'] == "UsernameExistsException":
+    #         print("Already signed with Kakao!")
+    #     else:
+    #         raise client_error
+
+    # body = {
+    #     "message": "Hello! Your email is %s" % user_email,
+    #     "input": event
+    # }
+
+    # return {"statusCode": 200, "body": json.dumps(body)}
