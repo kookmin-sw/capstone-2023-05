ad099ba04d1a2fa68e0bc0032362c07a861d8370
Jaewook-Lee <swljw@kookmin.ac.kr>
2023-03-07 03:37:38 +0900

    카카오톡 로그인 화면 주소는 얻었지만 로그인 하면 KOE101 에러 발생

--- backend/functions/handler.py
+++ backend/functions/handler.py
@@ -1,5 +1,6 @@
 import json
 import tqdm
+import requests
 
 
 def hello(event, context):
@@ -11,3 +12,23 @@ def hello(event, context):
     tqdm.tqdm()
 
     return {"statusCode": 200, "body": json.dumps(body)}
+
+
+def show_kakao_login(event, context):
+    """
+    Trying to login through Kakao account\n
+    You should replace those things below\n
+    - Kakao application: client_id(REST API Key), client_secret, redirect_uri\n
+    - AWS Cognito: ClientId(User Pool application client ID)\n
+    :return:
+    """
+
+    # Read under site
+    # https://www.linkedin.com/pulse/serverless-websites-aws-using-lambda-api-gateway-part-skultety
+
+    # Just giving Kakao login url
+    response = requests.get("https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=http://${CUSTOM_DOMAIN}/callback&response_type=code")
+    return {
+        "statusCode": 200,
+        "body": response.content.decode()
+    }
