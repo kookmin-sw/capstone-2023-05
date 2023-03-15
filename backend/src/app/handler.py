00c982da2328b58b85c1facd8b5b9c3da8b18997
Seungho Lee <poream3387@gmail.com>
2023-03-15 21:21:32 +0900

    implement create-room and get-room

--- backend/src/app/handler.py
+++ backend/src/app/handler.py
@@ -1,13 +1,39 @@
+import boto3
 import json
-import tqdm
+import psycopg2
 
 
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
-    }
 
-    tqdm.tqdm()
+    ENDPOINT = "naruhodoo-db.cldpkmn0dcie.ap-northeast-2.rds.amazonaws.com"
+    PORT = "5432"
+    USER = "root"
+    REGION = "ap-northeast-2"
+    DBNAME = "naruhodoo-db"
 
-    return {"statusCode": 200, "body": json.dumps(body)}
+    print("HELLOOOOOOOOOOOOO!!")
+    try:
+        client = boto3.client('rds')
+
+        token = client.generate_db_auth_token(
+            DBHostname=ENDPOINT, Port=PORT, DBUsername=USER, Region=REGION)
+
+        conn = psycopg2.connect(host=ENDPOINT, port=PORT, database=DBNAME,
+                                user=USER, password=token, sslrootcert="SSLCERTIFICATE")
+        cur = conn.cursor()
+        cur.execute("""SELECT now()""")
+        query_results = cur.fetchall()
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": query_results
+            })
+        }
+    except Exception as e:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "Error": e
+            })
+        }
