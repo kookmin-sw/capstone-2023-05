1477aee71ea93bd6d8f57b7fb5df9f0a24cf94d6
Jaewook-Lee <swljw@kookmin.ac.kr>
2023-04-08 18:33:36 +0900

    Trying to connect to PostgreSQL DB in docker.

--- backend/model.sql
+++ backend/model.sql
@@ -1,70 +1,66 @@
-CREATE TYPE "status_enum" AS ENUM (
-  'Not yet',
-  'Running',
-  'Finished'
-);
-
-CREATE TABLE "Opinion" (
-  "opinion_id" uuid PRIMARY KEY NOT NULL,
-  "player_id" uuid NOT NULL,
-  "team_id" uuid NOT NULL,
-  "voted_count" integer NOT NULL,
-  "round" integer NOT NULL,
-  "content" text NOT NULL,
-  "timestamp" timestamp NOT NULL
-);
-
-CREATE TABLE "Player" (
-  "player_id" uuid PRIMARY KEY NOT NULL,
-  "team_id" uuid NOT NULL,
-  "connect_id" varchar(50) NOT NULL
-);
-
-CREATE TABLE "Room" (
-  "room_id" uuid PRIMARY KEY NOT NULL,
-  "current_round" integer NOT NULL,
-  "status" status_enum NOT NULL
-);
-
-CREATE TABLE "RoomConfig" (
-  "config_id" varchar(50) PRIMARY KEY NOT NULL,
-  "owner_id" uuid NOT NULL,
-  "room_id" uuid NOT NULL,
-  "title" text NOT NULL,
-  "visibility" boolean NOT NULL,
-  "switch_chance" boolean NOT NULL,
-  "max_player_count" integer NOT NULL,
-  "end_round" integer NOT NULL,
-  "round_end_time" time NOT NULL,
-  "votes_per_user" integer NOT NULL,
-  "opinion_per_user" integer NOT NULL
-);
-
-CREATE TABLE "Team" (
-  "team_id" uuid PRIMARY KEY NOT NULL,
-  "room_id" uuid NOT NULL,
-  "image" bytea NOT NULL,
-  "name" varchar(50) NOT NULL
-);
-
+CREATE TYPE "status_enum" AS ENUM ('BEFORE_OPEN', 'RUNNING', 'CLOSED');
 CREATE TABLE "User" (
-  "user_id" uuid PRIMARY KEY NOT NULL,
-  "email" varchar(320) NOT NULL,
-  "nickname" varchar(50) NOT NULL,
-  "profile" varchar(50) NOT NULL,
-  "password" varchar(50) NOT NULL
+  userId uuid NOT NULL,
+  passwd bytea NOT NULL,
+  email varchar(50) NOT NULL,
+  nickname varchar(50) NOT NULL,
+  profile varchar(50) NULL,
+  CONSTRAINT PK_USER_ID PRIMARY KEY (userId)
 );
-
-ALTER TABLE "Opinion" ADD CONSTRAINT "FK_1" FOREIGN KEY ("player_id") REFERENCES "Player" ("player_id");
-
-ALTER TABLE "Opinion" ADD CONSTRAINT "FK_7" FOREIGN KEY ("team_id") REFERENCES "Team" ("team_id");
-
-ALTER TABLE "Player" ADD CONSTRAINT "FK_2" FOREIGN KEY ("player_id") REFERENCES "User" ("user_id");
-
-ALTER TABLE "Player" ADD CONSTRAINT "FK_3" FOREIGN KEY ("team_id") REFERENCES "Team" ("team_id");
-
-ALTER TABLE "RoomConfig" ADD CONSTRAINT "FK_4" FOREIGN KEY ("owner_id") REFERENCES "User" ("user_id");
-
-ALTER TABLE "RoomConfig" ADD CONSTRAINT "FK_5" FOREIGN KEY ("room_id") REFERENCES "Room" ("room_id");
-
-ALTER TABLE "Team" ADD CONSTRAINT "FK_6" FOREIGN KEY ("room_id") REFERENCES "Room" ("room_id");
+CREATE TABLE DiscussionBattle (
+  battleId uuid NOT NULL,
+  ownerId uuid NOT NULL,
+  title varchar(50) NOT NULL,
+  status status_enum NOT NULL,
+  visibility boolean NOT NULL,
+  switchChance boolean NOT NULL,
+  startTime timestamp NULL,
+  endTime timestamp NULL,
+  description varchar(50) NULL,
+  maxNoOfPlayers integer NOT NULL,
+  maxNoIfRounds integer NOT NULL,
+  maxNoVotes integer NOT NULL,
+  maxNoOfOpinion integer NOT NULL,
+  CONSTRAINT PK_DISCUSSION_BATTLE_ID PRIMARY KEY (battleId),
+  CONSTRAINT FK_8 FOREIGN KEY (ownerId) REFERENCES "User" (userId)
+);
+CREATE TABLE Round (
+  battleId uuid NOT NULL,
+  roundNo smallserial NOT NULL,
+  startTime timestamp NOT NULL,
+  endTime timestamp NULL,
+  description varchar(50) NULL,
+  CONSTRAINT PK_ROUND_ID PRIMARY KEY (battleId, roundNo),
+  CONSTRAINT FK_1 FOREIGN KEY (battleId) REFERENCES DiscussionBattle (battleId)
+);
+CREATE TABLE Opinion (
+  userId uuid NOT NULL,
+  battleId uuid NOT NULL,
+  roundNo smallserial NOT NULL,
+  "time" timestamp NOT NULL,
+  noOfLikes integer NOT NULL,
+  content varchar(50) NOT NULL,
+  status varchar(50) NOT NULL,
+  CONSTRAINT PK_OPINION_ID PRIMARY KEY (userId, battleId, roundNo, "time"),
+  CONSTRAINT FK_2 FOREIGN KEY (battleId, roundNo) REFERENCES Round (battleId, roundNo),
+  CONSTRAINT FK_3 FOREIGN KEY (userId) REFERENCES "User" (userId)
+);
+CREATE TABLE Team (
+  teamId uuid NOT NULL,
+  battleId uuid NOT NULL,
+  name varchar(50) NOT NULL,
+  image bytea NOT NULL,
+  CONSTRAINT PK_TEAM_ID PRIMARY KEY (teamId),
+  CONSTRAINT FK_4 FOREIGN KEY (battleId) REFERENCES DiscussionBattle (battleId)
+);
+CREATE TABLE Support (
+  userId uuid NOT NULL,
+  battleId uuid NOT NULL,
+  roundNo smallserial NOT NULL,
+  vote uuid NOT NULL,
+  "time" timestamp NOT NULL,
+  CONSTRAINT PK_SUPPORT_ID PRIMARY KEY (userId, battleId, roundNo),
+  CONSTRAINT FK_5 FOREIGN KEY (userId) REFERENCES "User" (userId),
+  CONSTRAINT FK_6 FOREIGN KEY (vote) REFERENCES Team (teamId),
+  CONSTRAINT FK_7 FOREIGN KEY (battleId, roundNo) REFERENCES Round (battleId, roundNo)
+);
