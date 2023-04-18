1e1fd48febb3333ce40a9ee5e4a61ff9ea9b5269
ChangHoon Sung <changhoon.dev@gmail.com>
2023-04-18 22:33:07 +0900

    Refactor database connection and handling

--- backend/src/lambda/handler.py
+++ backend/src/lambda/handler.py
@@ -10,10 +10,7 @@ from src.utility.context import PostgresContext
 from src.utility.decorator import cors
 
 
-dynamo_db = boto3.client('dynamodb')
-psql_ctx = PostgresContext(os.getenv("POSTGRES_HOST"), os.getenv("POSTGRES_PORT"), os.getenv("POSTGRES_USER"),
-                               os.getenv("POSTGRES_PASSWORD"), os.getenv("POSTGRES_DB"))
-psql_cursor = psql_ctx.cursor
+dynamo_db = boto3.client(**config.dynamo_db_config)
 
 
 @cors
@@ -70,7 +67,7 @@ def disconnect_handler(event, context):
 
     # Find request user's battle id
     response = dynamo_db.scan(
-        TableName="websocket-connections-jwlee-test",
+        TableName=config.DYNAMODB_WS_CONNECTION_TABLE,
         FilterExpression="connectionID = :connection_id",
         ExpressionAttributeValues={":connection_id": {"S": connection_id}},
         ProjectionExpression="battleID,connectionID"
@@ -78,7 +75,7 @@ def disconnect_handler(event, context):
     battle_id = response[0]['battleID']['S']
     
     dynamo_db.delete_item(
-        TableName="websocket-connections-jwlee-test",
+        TableName=config.DYNAMODB_WS_CONNECTION_TABLE,
         Key={
             'battleID': {'S': battle_id},
             'connectionID': {'S': connection_id}
@@ -102,7 +99,7 @@ def init_join_handler(event, context):
 
     # DynamoDB에 정보 등록
     dynamo_db.put_item(
-        TableName="websocket-connections-jwlee-test",
+        TableName=config.DYNAMODB_WS_CONNECTION_TABLE,
         Item={
             'connectionID': {'S': connection_id},
             'battleID': {'S': battle_id},
@@ -113,15 +110,13 @@ def init_join_handler(event, context):
     )
 
     # 어떤 팀이 있는지 RDS에서 정보 가져오기
-    select_query = f"SELECT name FROM Team WHERE battleid = \'{battle_id}\'"
-    psql_cursor.execute(select_query)
-    rows = psql_cursor.fetchall()
-    psql_cursor.close()
-    team_names = [row for row in rows]
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps({
+    with PostgresContext(**config.db_config) as psql_ctx:
+        with psql_ctx.cursor() as psql_cursor:
+            select_query = f"SELECT name FROM team WHERE battleid = \'{battle_id}\'"
+            psql_cursor.execute(select_query)
+            rows = psql_cursor.fetchall()
+            psql_cursor.close()
+            team_names = [row for row in rows]
             'message': 'Join Request Success',
             'teams': team_names
         })
@@ -134,7 +129,7 @@ def send_handler(event, context):
     # DynamoDB의 모든 값을 얻어온다.
     paginator = dynamo_db.get_paginator('scan')
     connections = []
-    for page in paginator.paginate(TableName="websocket-connections-jwlee-test"):
+    for page in paginator.paginate(TableName=config.DYNAMODB_WS_CONNECTION_TABLE):
         connections.extend(page['Items'])
 
     my_connection_id = event['requestContext']['connectionId']
@@ -172,10 +167,12 @@ def send_handler(event, context):
     round, num_of_likes = json.loads(event['body'])['round'], 0
     status = "CANDIDATE"
 
-    insert_query = f'INSERT INTO Opinion VALUES (\'{user_id}\', \'{battle_id}\', {round}, {opinion_time}, {num_of_likes}, \'{opinion}\', \'{status}\')'
-    psql_cursor.execute(insert_query)
-    psql_ctx.client.commit()
-    psql_cursor.close()
+    with PostgresContext(**config.db_config) as psql_ctx:
+        with psql_ctx.cursor() as psql_cursor:
+            insert_query = f'INSERT INTO Opinion VALUES (\'{user_id}\', \'{battle_id}\', {round}, {opinion_time}, {num_of_likes}, \'{opinion}\', \'{status}\')'
+            psql_cursor.execute(insert_query)
+            psql_ctx.client.commit()
+            psql_cursor.close()
 
     return {
         'statusCode': 200,
