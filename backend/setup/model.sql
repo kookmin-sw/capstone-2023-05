51089edd3e79fb2734b5b47134912938db66c1e0
Seungho Lee <poream3387@gmail.com>
2023-04-25 23:53:35 +0900

    Merged model.sql from feat/create-room

--- backend/setup/model.sql
+++ backend/setup/model.sql
@@ -1,5 +1,6 @@
 CREATE TYPE "battle_status" AS ENUM ('BEFORE_OPEN', 'RUNNING', 'CLOSED');
 CREATE TYPE "opinion_status" AS ENUM ('CANDIDATE', 'PUBLISHED', 'DROPPED', 'REPORTED');
+
 CREATE TABLE "User" (
   userId varchar(50) NOT NULL,
   passwd bytea NOT NULL,
@@ -17,8 +18,8 @@ CREATE TABLE DiscussionBattle (
   startTime timestamp NULL,
   endTime timestamp NULL,
   description varchar(100) NULL,
-  maxNoIfRounds integer NOT NULL,
-  maxNoVotes integer NOT NULL,
+  maxNoOfRounds integer NOT NULL,
+  maxNoOfVotes integer NOT NULL,
   maxNoOfOpinion integer NOT NULL,
   CONSTRAINT PK_DISCUSSION_BATTLE_ID PRIMARY KEY (battleId),
   CONSTRAINT FK_8 FOREIGN KEY (ownerId) REFERENCES "User" (userId)
@@ -26,7 +27,7 @@ CREATE TABLE DiscussionBattle (
 CREATE TABLE Round (
   battleId VARCHAR(6) NOT NULL,
   roundNo serial NOT NULL,
-  startTime timestamp NOT NULL,
+  startTime timestamp NULL,
   endTime timestamp NULL,
   description varchar(100) NULL,
   CONSTRAINT PK_ROUND_ID PRIMARY KEY (battleId, roundNo),
@@ -63,3 +64,26 @@ CREATE TABLE Support (
   CONSTRAINT FK_6 FOREIGN KEY (vote) REFERENCES Team (teamId),
   CONSTRAINT FK_7 FOREIGN KEY (battleId, roundNo) REFERENCES Round (battleId, roundNo)
 );
+
+
+INSERT INTO "User" (userId, passwd, nickname, profile) 
+VALUES ('poream3387@gmail.com', 'password', 'poream', 'www.naver.com');
+
+INSERT INTO DiscussionBattle(battleId, ownerId, title, status, visibility, switchChance, startTime, endTime, description, maxNoOfRounds, maxNoOfVotes, maxNoOfOpinion)
+VALUES('000001', 'poream3387@gmail.com', 'Door Vs Wheel', 'BEFORE_OPEN', TRUE, TRUE, NULL, NULL, 'Testing Battle Description', 3, 3, 3);
+
+INSERT INTO Team(teamId, battleId, name, image)
+VALUES(DEFAULT, '000001', 'Door', 'www.image.com');
+
+INSERT INTO Team(teamId, battleId, name, image)
+VALUES(DEFAULT, '000001', 'Wheel', 'www.image.com');
+
+Insert INTO Round(battleId, roundNo, startTime, endTime, description) 
+VALUES('000001', 1, NULL, NULL, NULL);
+
+Insert INTO Round(battleId, roundNo, startTime, endTime, description) 
+VALUES('000001', 2, NULL, NULL, NULL);
+
+Insert INTO Round(battleId, roundNo, startTime, endTime, description) 
+VALUES('000001', 3, NULL, NULL, NULL);
+
