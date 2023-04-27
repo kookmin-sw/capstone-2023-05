3ec92da93cd476b8cb270ed197341a35706dfd4b
0gyun <46589941+0gyun@users.noreply.github.com>
2023-04-27 22:05:50 +0900

    WIP: 프론트 추가 변경사항 (#57)
    Co-authored-by: younggyun <younggyun@YGMBP.local>

--- frontend/src/components/views/NavBar/InGameNavBar.js
+++ frontend/src/components/views/NavBar/InGameNavBar.js
@@ -10,25 +10,26 @@ function InGameNavBar(props) {
 
     });
   return (
-            <div className=" h-1/5">
-                <nav className='flex justify-start bg-battleBar bg-center bg-contain bg-no-repeat'>
-                    {/* <div className='text-lg px-8 py-6 leading-none bg-back-color text-white lg:mt-0 '>Naruhodoo</div>
-                    <div className="text-3xl py-3 align-middle h-full w-full text-center">문, 바퀴 뭐가 더 많을까?</div> */}
-                    <div className="flex flex-col w-full">
-                        <div id="flow" className="text-center items-center m-auto h-1/2 w-full pt-10 text-white">
-                            <span id="ready" className="px-3 py-2 border-2 border-white rounded-lg">준비</span>
-                            <pre className="inline px-2">&gt;</pre>
-                            <span id="open" className="px-3 py-2 border-white rounded-lg">공개</span>
-                            <pre className="inline px-2">&gt;</pre>
-                            <span id="vote" className="px-3 py-2 border-white rounded-lg">투표</span>
-                        </div>
-                        <div className="flex text-white text-4xl">
-                            <span id="a" className="flex-shrink-0 text-center py-12 ml-auto mr-auto">{teamA}</span>
-                            <span id="b" className="flex-shrink-0 text-center py-12 ml-auto mr-auto">{teamB}</span>
-                        </div>
-                    </div>
-                </nav>
-            </div>
+    // 비율같은 부분 추가 작업 필요
+    <div className=" h-window-card bg-battleBar bg-center bg-cover bg-no-repeat">
+        <nav className='h-full flex flex-col justify-center'>
+            {/* <div className="flex flex-col w-full h-full"> */}
+                <div id="flow" className="text-center items-center text-white mt-6">
+                    <span id="ready" className="px-3 py-2 border-2 border-white rounded-lg">준비</span>
+                    <pre className="inline px-2">&gt;</pre>
+                    <span id="open" className="px-3 py-2 border-white rounded-lg">공개</span>
+                    <pre className="inline px-2">&gt;</pre>
+                    <span id="vote" className="px-3 py-2 border-white rounded-lg">투표</span>
+                </div>
+                <br/>
+                <div className="flex text-white text-4xl">
+                    <span id="a" className="flex-shrink-0 text-center pb-2 ml-auto mr-auto">{teamA}</span>
+                    <span id="b" className="flex-shrink-0 text-center pb-2 ml-auto mr-auto">{teamB}</span>
+                </div>
+            {/* </div> */}
+        </nav>
+    </div>
+
   )
 }
 
