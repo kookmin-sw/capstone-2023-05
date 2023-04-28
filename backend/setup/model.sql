77c34c484046edd85b057a4e98eb7b3022f6420a
binsk-dev <juckmyeol0716@gmail.com>
2023-04-28 22:17:57 +0900

    🩹 Update model.sql for DiscussionBattle table

--- backend/setup/model.sql
+++ backend/setup/model.sql
@@ -2,92 +2,90 @@ CREATE TYPE "battle_status" AS ENUM ('BEFORE_OPEN', 'RUNNING', 'CLOSED');
 CREATE TYPE "opinion_status" AS ENUM ('CANDIDATE', 'PUBLISHED', 'DROPPED', 'REPORTED');
 
 CREATE TABLE "User" (
-  userId varchar(50) NOT NULL,
-  passwd bytea NOT NULL,
-  nickname varchar(50) NOT NULL,
+  "userId" varchar(50) NOT NULL,
+  "passwd" bytea NOT NULL,
+  "nickname" varchar(50) NOT NULL,
   profile varchar(50) NULL,
-  CONSTRAINT PK_USER_ID PRIMARY KEY (userId)
+  CONSTRAINT PK_USER_ID PRIMARY KEY ("userId")
 );
-CREATE TABLE DiscussionBattle (
-  battleId varchar(6) NOT NULL,
-  ownerId varchar(50) NOT NULL,
-  title varchar(50) NOT NULL,
+CREATE TABLE "DiscussionBattle" (
+  "battleId" varchar(6) NOT NULL,
+  "ownerId" varchar(50) NOT NULL,
+  "title" varchar(50) NOT NULL,
   status battle_status NOT NULL,
-  visibility boolean NOT NULL,
-  switchChance boolean NOT NULL,
-  startTime timestamp NULL,
-  endTime timestamp NULL,
+  "startTime" timestamp NULL,
+  "endTime" timestamp NULL,
   description varchar(100) NULL,
-  maxNoOfRounds integer NOT NULL,
-  maxNoOfVotes integer NOT NULL,
-  maxNoOfOpinion integer NOT NULL,
-  CONSTRAINT PK_DISCUSSION_BATTLE_ID PRIMARY KEY (battleId),
-  CONSTRAINT FK_8 FOREIGN KEY (ownerId) REFERENCES "User" (userId)
+  "refreshPeriod"  integer NOT NULL,
+  "maxNoOfRefresh" integer NOT NULL,
+  "maxNoOfRounds" integer NOT NULL,
+  "maxNoOfVotes" integer NOT NULL,
+  "maxNoOfOpinion" integer NOT NULL,
+  CONSTRAINT PK_DISCUSSION_BATTLE_ID PRIMARY KEY ("battleId"),
+  CONSTRAINT FK_8 FOREIGN KEY ("ownerId") REFERENCES "User" ("userId")
 );
-CREATE TABLE Round (
-  battleId VARCHAR(6) NOT NULL,
-  roundNo serial NOT NULL,
-  startTime timestamp NULL,
-  endTime timestamp NULL,
+CREATE TABLE "Round" (
+  "battleId" VARCHAR(6) NOT NULL,
+  "roundNo" serial NOT NULL,
+  "startTime" timestamp NULL,
+  "endTime" timestamp NULL,
   description varchar(100) NULL,
-  CONSTRAINT PK_ROUND_ID PRIMARY KEY (battleId, roundNo),
-  CONSTRAINT FK_1 FOREIGN KEY (battleId) REFERENCES DiscussionBattle (battleId)
+  CONSTRAINT PK_ROUND_ID PRIMARY KEY ("battleId", "roundNo"),
+  CONSTRAINT FK_1 FOREIGN KEY ("battleId") REFERENCES "DiscussionBattle" ("battleId")
 );
-CREATE TABLE Opinion (
-  userId varchar(50) NOT NULL,
-  battleId varchar(6) NOT NULL,
-  roundNo serial NOT NULL,
+CREATE TABLE "Opinion" (
+  "userId" varchar(50) NOT NULL,
+  "battleId" varchar(6) NOT NULL,
+  "roundNo" serial NOT NULL,
   "order" serial NOT NULL,
-  noOfLikes integer NOT NULL,
-  content varchar(150) NOT NULL,
+  "noOfLikes" integer NOT NULL,
+  "content" varchar(150) NOT NULL,
   "time" timestamp NOT NULL,
   status opinion_status NOT NULL,
-  CONSTRAINT PK_OPINION_ID PRIMARY KEY (userId, battleId, roundNo, "order"),
-  CONSTRAINT FK_2 FOREIGN KEY (battleId, roundNo) REFERENCES Round (battleId, roundNo),
-  CONSTRAINT FK_3 FOREIGN KEY (userId) REFERENCES "User" (userId)
+  CONSTRAINT PK_OPINION_ID PRIMARY KEY ("userId", "battleId", "roundNo", "order"),
+  CONSTRAINT FK_2 FOREIGN KEY ("battleId", "roundNo") REFERENCES "Round" ("battleId", "roundNo"),
+  CONSTRAINT FK_3 FOREIGN KEY ("userId") REFERENCES "User" ("userId")
 );
-CREATE TABLE Team (
-  teamId serial NOT NULL,
-  battleId varchar(6) NOT NULL,
+CREATE TABLE "Team" (
+  "teamId" serial NOT NULL,
+  "battleId" varchar(6) NOT NULL,
   name varchar(50) NOT NULL,
   image bytea NOT NULL,
-  CONSTRAINT PK_TEAM_ID PRIMARY KEY (teamId),
-  CONSTRAINT FK_4 FOREIGN KEY (battleId) REFERENCES DiscussionBattle (battleId)
+  CONSTRAINT PK_TEAM_ID PRIMARY KEY ("teamId"),
+  CONSTRAINT FK_4 FOREIGN KEY ("battleId") REFERENCES "DiscussionBattle" ("battleId")
 );
-CREATE TABLE Support (
-  userId varchar(50) NOT NULL,
-  battleId varchar(6) NOT NULL,
-  roundNo serial NOT NULL,
+CREATE TABLE "Support" (
+  "userId" varchar(50) NOT NULL,
+  "battleId" varchar(6) NOT NULL,
+  "roundNo" serial NOT NULL,
   vote serial NOT NULL,
   "time" timestamp NOT NULL,
-  CONSTRAINT PK_SUPPORT_ID PRIMARY KEY (userId, battleId, roundNo),
-  CONSTRAINT FK_5 FOREIGN KEY (userId) REFERENCES "User" (userId),
-  CONSTRAINT FK_6 FOREIGN KEY (vote) REFERENCES Team (teamId),
-  CONSTRAINT FK_7 FOREIGN KEY (battleId, roundNo) REFERENCES Round (battleId, roundNo)
+  CONSTRAINT PK_SUPPORT_ID PRIMARY KEY ("userId", "battleId", "roundNo"),
+  CONSTRAINT FK_5 FOREIGN KEY ("userId") REFERENCES "User" ("userId"),
+  CONSTRAINT FK_6 FOREIGN KEY (vote) REFERENCES "Team" ("teamId"),
+  CONSTRAINT FK_7 FOREIGN KEY ("battleId", "roundNo") REFERENCES "Round" ("battleId", "roundNo")
 );
 
 -- Local Testing Dummy Data
+INSERT INTO "User" ("userId", "passwd", "nickname", profile)
+VALUES 
+    ('joon@kookmin.ac.kr', E'\\x6a6f6f6e313233', 'lyj', 'www.example.com/joon/profile'),
+    ('hoon@kookmin.ac.kr', E'\\x686f6f6e313233', 'sch', 'www.example.com/hoon/profile'),
+    ('gyun@kookmin.ac.kr', E'\\x6779756e313233', 'lyg', 'www.example.com/gyun/profile'),
+    ('ho@kookmin.ac.kr', E'\\x686f313233', 'lsh', 'www.example.com/ho/profile'),
+    ('wuk@kookmin.ac.kr', E'\\x77756b313233', 'ljw', 'www.example.com/ljw/profile');
 
-INSERT INTO "User" (userId, passwd, nickname, profile) 
-VALUES ('newuser@gmail.com', 'password', 'abc', 'www.naver.com');
+INSERT INTO "DiscussionBattle" ("battleId", "ownerId", "title", status, "startTime", "endTime", description, "refreshPeriod", "maxNoOfRefresh", "maxNoOfRounds", "maxNoOfVotes", "maxNoOfOpinion")
+VALUES 
+    ('ABC123', 'joon@kookmin.ac.kr', 'Which is many? door or wheel', 'BEFORE_OPEN', '2023-05-01 12:00:00', '2023-05-31 12:00:00', 'Our first battle', 300, 10, 3, 10, 50);
 
-INSERT INTO DiscussionBattle(battleId, ownerId, title, status, visibility, switchChance, startTime, endTime, description, maxNoOfRounds, maxNoOfVotes, maxNoOfOpinion)
-VALUES('000002', 'newuser@gmail.com', 'Door Vs Wheel', 'BEFORE_OPEN', TRUE, TRUE, NULL, NULL, 'Testing Battle Description', 3, 3, 3);
+INSERT INTO "Round" ("battleId", "startTime", "endTime", "description")
+VALUES 
+    ('ABC123', '2023-05-01 12:00:00', '2023-05-07 12:00:00', 'Initial round'),
+    ('ABC123', '2023-05-08 12:00:00', '2023-05-14 12:00:00', 'Second round'),
+    ('ABC123', '2023-05-15 12:00:00', '2023-05-21 12:00:00', 'Final round');
 
-INSERT INTO Team(teamId, battleId, name, image)
-VALUES(DEFAULT, '000002', 'Door', 'www.image.com');
-
-INSERT INTO Team(teamId, battleId, name, image)
-VALUES(DEFAULT, '000002', 'Wheel', 'www.image.com');
-
-Insert INTO Round(battleId, roundNo, startTime, endTime, description) 
-VALUES('000002', 1, NULL, NULL, NULL);
-
-Insert INTO Round(battleId, roundNo, startTime, endTime, description) 
-VALUES('000002', 2, NULL, NULL, NULL);
-
-Insert INTO Round(battleId, roundNo, startTime, endTime, description) 
-VALUES('000002', 3, NULL, NULL, NULL);
-
-INSERT INTO Opinion (userId, battleId, roundNo, noOfLikes, content, "time", status)
-VALUES ('newuser@gmail.com', '000002', 1, 0, '첫번째 의견입니다.', '2023-04-27 10:00:00', 'CANDIDATE');
+INSERT INTO "Team" ("battleId", "name", "image")
+VALUES
+    ('ABC123', 'Team Door', '0123456789ABCDEF'),
+    ('ABC123', 'Team Wheel', 'FEDCBA9876543210');
