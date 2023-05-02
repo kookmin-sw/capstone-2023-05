17f9b5b4b695f86dc971e5ee80832a77c6f1fe2b
binsk-dev <juckmyeol0716@gmail.com>
2023-05-02 21:53:16 +0900

    ✨ Add like feature

--- backend/src/lambda/handler.py
+++ backend/src/lambda/handler.py
@@ -275,3 +275,78 @@ def vote_handler(event, context, wsclient):
         'body': 'Vote Success'
     }
     return response
+
+@wsclient
+def like_handler(event, context, wsclient):
+    connection_id = event['requestContext']['connectionId']
+
+    # DynamoDB에서 유저 정보 찾기
+    response = dynamo_db.scan(
+        TableName=config.DYNAMODB_WS_CONNECTION_TABLE,
+        FilterExpression="connectionID = :connection_id",
+        ExpressionAttributeValues={":connection_id": {"S": connection_id}},
+        ProjectionExpression="battleID,connectionID,nickname,userID"
+    )['Items']
+    
+    
+    battle_id = response[0]['battleID']['S']
+    user_id = json.loads(event['body'])['userId']
+    order = json.loads(event['body'])['opinionNo']
+    round = json.loads(event['body'])['round']
+    
+    # Opinion 테이블에서 해당 의견의 좋아요 수를 1증가 시킨다.
+    # Race condition이 있기 때문에 쿼리를 동기화 해야한다.
+    try:
+        with PostgresContext(**config.db_config) as psql_ctx:
+            with psql_ctx.cursor() as psql_cursor:
+                psql_cursor.execute("BEGIN")
+                psql_cursor.execute(f'SELECT "noOfLikes" FROM "Opinion" WHERE "userId" = \'{user_id}\' AND "battleId" = \'{battle_id}\' AND "roundNo" = {round} AND "order" = {order} FOR UPDATE;')
+                row = psql_cursor.fetchone()      
+                likes = row[0]
+                psql_cursor.execute(f'UPDATE "Opinion" SET "noOfLikes" = {likes + 1} WHERE "userId" = \'{user_id}\' AND "battleId" = \'{battle_id}\' AND "roundNo" = {round} AND "order" = {order}')
+                psql_cursor.execute("COMMIT;")
+    except:
+        wsclient.send(
+            connection_id=connection_id,
+            data={
+                "action": "likeResult",
+                "result": "fail",
+                "opinionNo": order,
+                "noOfLikes": "None"
+            }
+        )
+
+        response = {
+            'statusCode': 400,
+            'body': 'Like Fail'
+        }
+        return response
+    
+    # Opinion 테이블에서 해당 의견의 좋아요 수를 가져온다.
+    with PostgresContext(**config.db_config) as psql_ctx:
+        with psql_ctx.cursor() as psql_cursor:
+            select_query = f"""
+                SELECT "noOfLikes"
+                FROM "Opinion"
+                WHERE "userId" = '{user_id}' AND "battleId" = '{battle_id}' AND "roundNo" = {round} AND "order" = {order}
+            """
+            psql_cursor.execute(select_query)
+            row = psql_cursor.fetchall()
+            no_of_likes = row[0][0]
+    
+    # Response 전송
+    wsclient.send(
+        connection_id=connection_id,
+        data={
+            "action": "likeResult",
+            "result": "success",
+            "opinionNo": order,
+            "noOfLikes": no_of_likes
+        }
+    )
+
+    response = {
+        'statusCode': 200,
+        'body': 'Like Success'
+    }
+    return response
