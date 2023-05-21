253bfb04bb959f02b777debdb6f93f682c4d1a92
0gyun <46589941+0gyun@users.noreply.github.com>
2023-05-21 19:59:28 +0900

    Front/feat/teams opinions (#73)
    Co-authored-by: younggyun <younggyun@YGMBP.local>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,10 +7,12 @@ import {
 } from "react-router-dom";
 
 import LandingPage from './components/views/LandingPage/LandingPage'
-import LoginPage from './components/views/LoginPage/LoginPage'
+import TeamsOpinions from './components/views/cardView/TeamsOpinions'
 import MakingRoomPage from './components/views/MakingRoomPage/MakingRoomPage'
 import MyPage from './components/views/MyPage/MyPage'
 import BattlePage from './components/views/BattlePage/BattlePage'
+import BattlePageHost from './components/views/BattlePage/BattlePageHost'
+import BeforeStart from './components/views/BeforeStart/BeforeStart'
 
 class App extends Component {
   render(){
@@ -20,10 +22,12 @@ class App extends Component {
           <Routes>
           <Route exact path="/" element={<LandingPage />} />
             {/* <Route path="/makeRoom" element={<MakingRoomPage />} /> */}
-            <Route path="/login" element={<LoginPage />} />
+            <Route path="/host" element={<BattlePageHost />} />
             <Route path="/makeRoom" element={<MakingRoomPage />} />
             <Route path="/mypage" element={<MyPage />} />
             <Route path="/game" element={<BattlePage />} />
+            <Route path="/before" element={<BeforeStart />} />
+            <Route path="/teamsOpinions" element={<TeamsOpinions />} />
           </Routes>
         </Router>
       </>
